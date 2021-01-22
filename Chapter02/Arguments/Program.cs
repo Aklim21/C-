6dcf5e93b1d5e49cb20722d53c506125f32b1742
Aklim21 <anthonyjim101@gmail.com>
2021-01-22 18:36:12 +0000

    update

--- Chapter02/Arguments/Program.cs
+++ Chapter02/Arguments/Program.cs
@@ -7,11 +7,24 @@ namespace Arguments
     {
         static void Main(string[] args)
         {
-            WriteLine($"There are {args.Length} arguments");
-            foreach(string arg in args){
-                WriteLine(arg);
-                
+            if (args.Length < 4){
+                WriteLine("Please input two colours and dimensions");
+                WriteLine("e.g.: dotnet run red yellow 80 40");
+                return;
             }
+            ForegroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[0],
+                ignoreCase: true);
+
+            BackgroundColor = (ConsoleColor)Enum.Parse(
+                enumType: typeof(ConsoleColor),
+                value: args[1],
+                ignoreCase: true);
+
+            WindowWidth = int.Parse(args[2]);
+            WindowHeight = int.Parse(args[3]);
         }
     }
 }
+
