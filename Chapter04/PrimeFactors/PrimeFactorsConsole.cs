02d1d4b97f91638872978a1d3d322c243c1fe27b
Aklim21 <anthonyjim101@gmail.com>
2021-01-30 12:34:30 +0000

    commit

--- Chapter04/PrimeFactors/PrimeFactorsConsole.cs
+++ Chapter04/PrimeFactors/PrimeFactorsConsole.cs
@@ -8,9 +8,23 @@ namespace PrimeFactors
     {
         static void Main(string[] args)
         {
-            WriteLine("Enter a value:");
-            int input = int.TryParse(ReadLine(),out int);
+            var getPrime = new PrimeFunctions();
 
+            bool isNumber = false;
+            while(isNumber == false)
+            {
+                WriteLine("Enter a positive value up to 1000:");
+                isNumber = int.TryParse(ReadLine(),out int number);
+                if(isNumber == true && number>0 && number<=1000)
+                {
+                    WriteLine(getPrime.getPrimeFact(number));
+                }
+                else
+                {
+                    WriteLine("You fucked it, try again.");
+                    isNumber = false;
+                }
+            }
         }
     }
 }
