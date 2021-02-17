410545de0766fb724ae20c78bf5f65ac467d0cfb
Aklim21 <anthonyjim101@gmail.com>
2021-02-17 19:01:58 +0000

    commit

---
+++ Chapter06/Ch06_App/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Ch06_Library;
+using static System.Console;
+
+namespace Ch06_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ant = new Person()
+            {
+                szName = "Anthony",
+                dtDateofBirth = new DateTime(1996, 06, 17)
+
+            };
+            
+            //new Person instances
+            Person dave = new Person{szName = "David"};
+            Person gol = new Person{szName = "Goliath"};
+            Person mossy = new Person{szName = "Moses"};
+
+            //Call static Method:
+            var daliath = Person.Woohoo(dave,gol);
+
+            // WriteLine(
+            //     $"{dave.szName} has {dave.Children.Count} child(ren).\n{gol.szName} has {gol.Children.Count} child(ren)."
+            // );
+
+            // WriteLine($"{dave.szName} and {gol.szName} have a child called: \"{daliath.szName}\".");
+
+            //Operator function
+            // var gavid = dave *gol;
+            // WriteLine(
+            //     $"{dave.szName} has {dave.Children.Count} child(ren).\n{gol.szName} has {gol.Children.Count} child(ren)."
+            // );
+            // gavid.szName = "Gavid";
+            // WriteLine($"{dave.szName} and {gol.szName} have a child called: \"{gavid.szName}\".");
+
+            /*----------------------------------*/
+            //Local functions
+
+            var x = 4;
+            var y = math.Factorial(x);
+            // WriteLine($"The factorial of {x} is {y}");
+            
+            /*----------------------------------*/
+            //Delegates
+
+            int testString = math.CalledMethod("Welcome");
+            // WriteLine(testString);
+
+            var d = new math.DelegateMethodCall(math.CalledMethod);
+            var testString2 = d("Welcome");
+            // WriteLine(testString2);
+
+
+            /*----------------------------------*/
+            //Set Events and delegates
+            ant.Shout = ant_Shout;
+            ant.Poke();
+            ant.Poke();
+            ant.Poke();
+            ant.Poke();
+
+        }
+        public static void ant_Shout(object sender, EventArgs e)
+        {
+            Person p = (Person)sender;
+            WriteLine($"{p.szName} is level {p.AngerLevel} angry!");
+        }
+        
+    }
+}
---
+++ Chapter06/Ch06_Library/Person.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+
+namespace Ch06_Library
+{
+    public class Person
+    {
+        //Fields
+        public string szName;
+        public DateTime dtDateofBirth;
+        public List<Person> Children = new List<Person>();
+
+        //static Method
+
+        public static Person Woohoo(Person x, Person y)
+        {
+            var baby = new Person
+            {
+                szName = $"Baby of {x.szName} and {y.szName}"
+            };
+
+            x.Children.Add(baby);
+            y.Children.Add(baby);
+
+            return baby;
+        }
+
+        //instance Method
+
+        public Person WoohooWith(Person spouse)
+        {
+            return Woohoo(this, spouse);
+        }
+
+        public static Person operator * (Person x, Person y)
+        {
+            return Person.Woohoo(x,y);
+        }
+
+
+        /*----------------------------------*/
+        //Delegates and events: 
+        
+        //define the delegate field for each person
+        public EventHandler Shout;
+
+        //define the AngerLevel field for each person
+        public int AngerLevel;
+        //define the method to be called on event activation
+        public void Poke()
+        {
+            AngerLevel++;
+            if (AngerLevel >= 3)
+            {
+                //if something is listening...
+
+                // if (Shout != null)
+                // {
+                //     Shout(this, EventArgs.Empty);
+                // }
+                Shout?.Invoke(this, EventArgs.Empty);
+
+            }
+        }
+    
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    /*----------------------------------*/
+    //Local functions
+    public class math
+    {
+        public static int Factorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException($"Number has to be larger that 0!");
+            }
+            return LocalFactorial(number);
+
+            int LocalFactorial(int localNumber)
+            {
+                if (localNumber < 1) return 1;
+                return localNumber * LocalFactorial(localNumber - 1);
+            }
+        }
+
+        /*----------------------------------*/
+        //Delegates
+
+        public static int CalledMethod(string input)
+        {
+            return input.Length;
+        }
+        public delegate int DelegateMethodCall(string s);
+
+
+
+
+    
+        
+
+
+
+
+
+
+
+
+
+    }
+}
